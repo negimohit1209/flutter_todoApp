226de4d01d95767609081b1311f67f396e98713d
negimohit1209 <mohitsngh1997@gmail.com>
2019-06-27 17:46:49 +0530

    add alert dialog for the input purpose

--- lib/ui/notodo_screen.dart
+++ lib/ui/notodo_screen.dart
@@ -1,3 +1,4 @@
+import 'package:database_intro/models/nodo.dart';
 import 'package:flutter/material.dart';
 
 class NotodoScreen extends StatefulWidget {
@@ -7,6 +8,14 @@ class NotodoScreen extends StatefulWidget {
 }
 
 class _NotodoScreenState extends State<NotodoScreen> {
+  final TextEditingController _textFieldController = TextEditingController();
+
+
+  void _handleSubmitted(String text) async {
+    _textFieldController.clear();
+    NodoItem nodoItem = NodoItem(text, DateTime.now().toIso8601String());
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,9 +34,38 @@ class _NotodoScreenState extends State<NotodoScreen> {
         ),
         onPressed: _showFormDialog,
       ),
-      
     );
   }
 
-  void _showFormDialog() {}
+  void _showFormDialog() {
+    var alert = AlertDialog(
+      content: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextField(
+              controller: _textFieldController,
+              autofocus: true,
+              decoration: InputDecoration(
+                  labelText: "Item",
+                  hintText: 'eg. dont drink beer',
+                  icon: Icon(Icons.note_add)),
+            ),
+          ),
+        ],
+      ),
+      actions: <Widget>[
+        FlatButton(
+            onPressed: () {
+              _handleSubmitted(_textFieldController.text);
+              _textFieldController.clear();
+            },
+            child: Text('save')),
+        FlatButton(onPressed: () => Navigator.pop(context) , 
+        child: Text('cancel'),)
+      ],
+    );
+    showDialog(context: context, builder: (_){
+      return alert;
+    });
+  }
 }
